6403f4070d7d6df788219a958452acb38e77add2
bit-zll <524183259@qq.com>
2020-09-28 22:17:42 +0800

    基础IO2

---
+++ linux-lesson12/sysfileoper/sysfileoper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+int main()
+{
+    close(0);
+    int ret = -1;    
+    ret = open("./linux84",O_RDWR|O_CREAT,0664);
+    if(ret<0)
+    {
+        return -1;   
+    }
+    printf("file oper ret = %d\n",ret);
+    return 0;
+}
